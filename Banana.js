3045cf7a1522c729d244abc0b15c033809a5dc24
GirishRed <71594099+GirishRed@users.noreply.github.com>
2020-11-30 13:44:30 +0530

    ver 1.1

--- Banana.js
+++ Banana.js
@@ -1,31 +1,28 @@
+var btntranslate =document.querySelector("#btn-translator");
+
 var txtInput = document.querySelector("#txt-input" );
 
 var outputDiv = document.querySelector("#output");
 
-var serverURL = "https://api.funtranslations.com/translate/minion.json" ;
+// var serverURL =  "https://api.funtranslations.com/translate/"  
+var serverURL= "http://lessonfourapi.tanaypratap.repl.co/translate/yoda.json"
 
 function getTranslationURL(input) {
     return serverURL+ "?" + "text=" + input
 }
-
 function errorHandling(error){
     consle.log("Hey Some Error has occured",error);
     alert("Please Try Again after some time")
 }
-
-
 function clickHandler(){
 var inputText =txtInput.value;
-
 fetch(getTranslationURL(inputText))
-
-    .then(response ==> resonse.json())
-    .then(json ==> {
+    .then(response => resonse.json())
+    .then(json =>  { 
         var translatedText =json.contents.translated;
         outputDiv.innerText = transalatedText;
 
     })
     .catch(errorHandler)
 };
-
-btntranslate.addEventListener("click", clickHandler)
+btntranslate.addEventListener("click", clickHandler);
