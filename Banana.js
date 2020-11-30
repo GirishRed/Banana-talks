95ffac6dacb5efc060e663b141b607a2f6b49425
GirishRed <71594099+GirishRed@users.noreply.github.com>
2020-11-30 14:09:11 +0530

    ver 1.3 code is done, nxt design

--- Banana.js
+++ Banana.js
@@ -1,26 +1,26 @@
-var btntranslate =document.querySelector("#btn-translator");
+var btntranslate =document.querySelector("#btn-translate");
 
 var txtInput = document.querySelector("#txt-input" );
 
 var outputDiv = document.querySelector("#output");
 
-// var serverURL =  "https://api.funtranslations.com/translate/"  
-var serverURL= "http://lessonfourapi.tanaypratap.repl.co/translate/yoda.json"
+ var serverURL =  "https://api.funtranslations.com/translate/"  
+// var serverURL= "http://lessonfourapi.tanaypratap.repl.co/translate/yoda.json"
 
 function getTranslationURL(input) {
     return serverURL+ "?" + "text=" + input
 }
-function errorHandling(error){
-    consle.log("Hey Some Error has occured",error);
-    alert("Please Try Again after some time")
+function errorHandler(error){
+    console.log("Hey Some Error has occured",error);
+    alert("Please Try Again after some time");
 }
 function clickHandler(){
 var inputText =txtInput.value;
 fetch(getTranslationURL(inputText))
-    .then(response => resonse.json())
+    .then(response => response.json())
     .then(json =>  { 
         var translatedText =json.contents.translated;
-        outputDiv.innerText = transalatedText;
+        outputDiv.innerText = translatedText;
 
     })
     .catch(errorHandler)
