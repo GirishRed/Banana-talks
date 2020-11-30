5663e36eb4526ade797cc1a2bbc03034d083b451
GirishRed <71594099+GirishRed@users.noreply.github.com>
2020-11-30 11:33:26 +0530

    ver 1.0

---
+++ Banana.js
@@ -0,0 +1,31 @@
+var txtInput = document.querySelector("#txt-input" );
+
+var outputDiv = document.querySelector("#output");
+
+var serverURL = "https://api.funtranslations.com/translate/minion.json" ;
+
+function getTranslationURL(input) {
+    return serverURL+ "?" + "text=" + input
+}
+
+function errorHandling(error){
+    consle.log("Hey Some Error has occured",error);
+    alert("Please Try Again after some time")
+}
+
+
+function clickHandler(){
+var inputText =txtInput.value;
+
+fetch(getTranslationURL(inputText))
+
+    .then(response ==> resonse.json())
+    .then(json ==> {
+        var translatedText =json.contents.translated;
+        outputDiv.innerText = transalatedText;
+
+    })
+    .catch(errorHandler)
+};
+
+btntranslate.addEventListener("click", clickHandler)
