afdaf91c7d822a2bc142bf936e995f8dc20599cf
GirishRed <71594099+GirishRed@users.noreply.github.com>
2020-12-09 22:21:20 +0530

    final touch 1.0

--- Banana.js
+++ Banana.js
@@ -4,8 +4,8 @@ var txtInput = document.querySelector("#txt-input");
 
 var outputDiv = document.querySelector("#output");
 
-var serverURL = "https://api.funtranslations.com/translate/"
-// var serverURL= "http://lessonfourapi.tanaypratap.repl.co/translate/yoda.json"
+var serverURL = "https://api.funtranslations.com/translate/minion.json"
+ //var serverURL= "http://lessonfourapi.tanaypratap.repl.co/translate/yoda.json"
 
 function getTranslationURL(input) {
     return serverURL + "?" + "text=" + input
